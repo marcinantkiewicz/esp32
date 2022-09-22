f9ee1d922e1945fdbaf062202f48801fa44e215c
user <user@pop-os.localdomain>
2022-09-21 22:09:36 -0500

    Adding humidity monitor

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # esp32
  - fermenter_controller: instrumentation for 22gal fermenter. Status: thermocouple reads 10F high. Heating pad relay works.
+ - humidity_monitor: dht22 based logger. Functional, hardcodes everything. Requires data logging service.
--- humidity_monitor/dht_first.ino
+++ humidity_monitor/dht_first.ino
@@ -1,25 +1,22 @@
+#include <DHT.h>
 #include <HTTPClient.h>
-#include "max6675.h"
 
-#define relay 16
-#define SCK 15
-#define CS 14
-#define SO 13
-
-MAX6675 tc(SCK, CS, SO);
+#define DHTPIN 16
+#define DHTTYPE DHT22
 
 const char* ssid = SSID;
-const char* password = PW_HERE;
+const char* password = PASSWORD;
 const char* server = "http://192.168.1.50:3003/sensors/data";
 const int report_interval = 60;
 
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
 
   Serial.print("Connecting to WiFi");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED){
     Serial.print('.');
     sleep(1);
   }
@@ -33,26 +30,27 @@ void setup() {
   Serial.print("Sending to server at: ");
   Serial.println(server);
 
-  pinMode(relay, OUTPUT);
+  dht.begin();
 }
 
-void httpGetRequest(const char* server, float h, float t) {
+void httpGetRequest(const char* server, float h, float t){
   HTTPClient http;
   const char* schema = "dht";
   const char* sensor_name = "dht1";
   char url[100];
-  sprintf(url, "%s/dht?sn=%s&h=%d&t=%d", server, sensor_name, (int)h, (int)t);
+  sprintf(url, "%s/dht?sn=%s&h=%d&t=%d",server,sensor_name,(int)h,(int)t);
   http.begin(url);
   Serial.print("url: ");
   Serial.println(url);
-
+  
   int httpResponseCode = http.GET();
-
-  if (httpResponseCode == 200) {
+  
+  if (httpResponseCode == 200){
     Serial.println('url is:' + url);
     Serial.println("HTTP Response Code: ");
     Serial.println(httpResponseCode);
-  } else {
+  }
+  else {
     Serial.println(url);
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
@@ -64,11 +62,21 @@ int counter = 0;
 void loop() {
   Serial.print("Counter: ");
   Serial.println(counter++);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-  digitalWrite(relay, LOW);
-  sleep(1);
-  digitalWrite(relay, HIGH);
-  sleep(2);
-
-  Serial.println(tc.readFahrenheit());
+  if(isnan(t) || isnan(h)){
+    Serial.println("no temp or hum");
+    httpGetRequest(server,0,0);
+    sleep(report_interval);
+    return;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temp: ");
+  Serial.print(t);
+  Serial.println(" C");
+  httpGetRequest(server, h, t);
+  sleep(report_interval);
 }
